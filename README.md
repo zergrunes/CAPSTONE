dd831f56fb054b3d557e5e11ee8bdf562f517cdf
Marian Remoroza <mrremoroza@cpp.edu>
2022-12-12 16:44:36 -0800

    im done

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-It's about squirrels.
+Powerpoint: https://docs.google.com/presentation/d/1WITN0JluzQim7dHHpF2YOXUS_jcc7Ek4f1Xr2OU_VY0/edit#slide=id.g142ee4901a3_0_104
+
+The Squirrel Census is a multimedia science, design, and storytelling project focusing on the Eastern gray (Sciurus carolinensis). They count squirrels and present their findings to the public.
+
+On March 1, 2020 — with the help of 72 volunteer Squirrel Sighters, as well as NYC Open Data — they performed a sample count in 24 New York City parks, and gathered other material data. Four hundred and thirty-three squirrel sightings were tallied. The methodology was less focused on total squirrel numbers per hectare and more attuned to the stories — of squirrels, humans, and parks.
--- squirrel.py
+++ squirrel.py
@@ -4,15 +4,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 sns.set_style('darkgrid')
 sns.color_palette('hls', 8)
-sq = pd.read_csv('squirrel-data.csv')
-pk = pd.read_csv('park-data.csv', 'rb', encoding_errors='ignore')
 graph1 = pd.read_csv('graph1.csv')
 graph2 = pd.read_csv('graph2.csv')
-
-# first graph - number of squirrel sighting per park + their activities
-sns.barplot(data=graph1, x='Park_ID', y='Number of Squirrels')
+graph3 = pd.read_csv('graph3.csv')
+# first graph - number of squirrel sighting per park
+x = sns.barplot(data=graph1, x='Park_ID', y='Number of Squirrels')
 plt.show()
-sns.kdeplot(data=graph2, x='Park ID', hue='Activities',
-            multiple='stack', palette='hls')
-plt.legend(bbox_to_anchor=(1.02, 1), loc='upper right', title='Activities')
+# second graph - squirrels and activities throughout the park
+y = sns.scatterplot(data=graph2, x='Park ID', y='Activity', hue='Activities')
+plt.legend(bbox_to_anchor=(1.5, 2), loc='upper right', title='Activities')
+plt.show()
+# third graph - time and temperature
+g3 = graph3.plot.hexbin(x='Park ID',
+                        y='Temperature',
+                        C='Total Time',
+                        reduce_C_function=np.sum,
+                        gridsize=10,
+                        cmap='GnBu')
 plt.show()
